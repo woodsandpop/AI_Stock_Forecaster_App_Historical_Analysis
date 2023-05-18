38bc8990ce1064e677c0ec1dc90cd09ce20f90d8
woodsandpop <132575779+woodsandpop@users.noreply.github.com>
2023-05-18 09:40:59 +0100

    Update stocktimeseries.py

--- stocktimeseries.py
+++ stocktimeseries.py
@@ -3,7 +3,7 @@ import streamlit as st
 from datetime import date
 
 import yfinance as yf
-from prophet import prophet
+from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 
